fffddf0f9fb803fbff35a9b3417b80368a5268a6
juvilla <92930948+juvilla@users.noreply.github.com>
2022-05-24 17:32:20 -0500

    Add files via upload

--- futbolista.py
+++ futbolista.py
@@ -2,7 +2,7 @@ from deportista import Deportista
 from persona import Persona
 class Futbolista(Deportista):
     _listaFutbolistas=[]
-    def __init__(self,nombre,edad,altura,sexo,añosPracticando,golesMarcados,piernaHabil,tarjetasRojas):
+    def __init__(self,nombre,edad,altura,sexo,añosPracticando,golesMarcados,tarjetasRojas,piernaHabil):
         super().__init__(self,nombre,edad,altura,sexo,"Futbol",añosPracticando)
         self._golesMarcados=golesMarcados
         self._tarjetasRojas=tarjetasRojas
